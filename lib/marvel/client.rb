e6e20f57959b1e3312268c87868b54c97efb1653
Rahul Horé <hore.rahul@gmail.com>
2014-02-01 22:10:30 -0500

    Stubs out all publicly available API requests

--- lib/marvel/client.rb
+++ lib/marvel/client.rb
@@ -9,5 +9,195 @@ module Marvel
     def initialize
       reset
     end
+
+    # TODO; MODULARIZE THIS!!!
+
+    # Characters: 
+
+    # fetches lists of characters
+    def get_characters
+      # v1/public/characters
+    end
+
+    # fetches a single character by id
+    def get_character
+      # v1/public/characters/{characterId}
+    end
+
+    # fetches lists of comics filtered by a character id
+    def get_comics_by_character_id
+      # v1/public/characters/{characterId}/comics
+    end
+
+    # fetches lists of events filtered by a character id
+    def get_events_by_character_id
+      # v1/public/characters/{characterId}/events
+    end
+
+    # fetches lists of stories filtered by a character id
+    def get_stories_by_character_id
+      # v1/public/characters/{characterId}/stories
+    end
+
+    # Comics:
+
+    # fetches lists of comics
+    def get_comics
+      # v1/public/comics
+    end
+
+    # fetches a single comic by id
+    def get_comic
+      # v1/public/comics/{comicId}
+    end
+
+    # fetches lists of characters filtered by a comic id
+    def get_characters_by_comic_id
+      # v1/public/comics/{comicId}/characters
+    end
+
+    # fetches lists of creators filtered by a comic id
+    def get_creators_by_comic_id
+      # v1/public/comics/{comicId}/creators
+    end
+
+    # fetches lists of events filtered by a comic id
+    def get_events_by_comic_id
+      # v1/public/comics/{comicId}/events
+    end
+
+    # fetches lists of stories filtered by a comic id
+    def get_stories_by_comic_id
+      # v1/public/comics/{comicId}/stories
+    end
+
+    # Creators:
+
+    # fetches lists of creators
+    def get_creators
+      # v1/public/creators
+    end
+
+    # fetches a single creator by id
+    def get_creator
+      # v1/public/creators/{creatorId}
+    end
+
+    # fetches lists of comics filtered by a creator id
+    def get_comics_by_creator_id
+      # v1/public/creators/{creatorId}/comics
+    end
+
+    # fetches lists of events filtered by a creator id
+    def get_events_by_creator_id
+      # v1/public/creators/{creatorId}/events
+    end
+
+    # fetches lists of stories filtered by a creator id
+    def get_stories_by_creator_id
+      # v1/public/creators/{creatorId}/stories
+    end
+
+    # Events:
+
+    # fetches lists of events
+    def get_events
+      # v1/public/events
+    end
+
+    # fetches a single event by id
+    def get_event
+      # v1/public/events/{eventId}
+    end
+
+     # fetches lists of characters filtered by an event id
+    def get_characters_by_event_id
+      # v1/public/events/{eventId}/characters
+    end
+
+    # fetches lists of comics filtered by an event id
+    def get_comics_by_event_id
+      # v1/public/events/{eventId}/comics
+    end
+
+    # fetches lists of creators filtered by an event id
+    def get_creators_by_event_id
+      # v1/public/events/{eventId}/creators
+    end
+
+    # fetches lists of stories filtered by an event id
+    def get_stories_by_event_id
+      # v1/public/events/{eventId}/stories
+    end
+
+
+    # Series:
+
+    # fetches lists of series
+    def get_series
+      # v1/public/series
+    end
+
+    # fetches a single comic series by id
+    def get_series
+      # v1/public/series/{seriesId}
+    end
+
+     # fetches lists of characters filtered by a series id
+    def get_characters_by_series_id
+      # v1/public/series/{seriesId}/characters
+    end
+
+    # fetches lists of comics filtered by a series id
+    def get_comics_by_series_id
+      # v1/public/series/{seriesId}/comics
+    end
+
+    # fetches lists of creators filtered by a series id
+    def get_creators_by_series_id
+      # v1/public/series/{seriesId}/creators
+    end
+
+    # fetches lists of events filtered by a series id
+    def get_events_by_series_id
+      # v1/public/series/{seriesId}/events
+    end
+
+    # fetches lists of stories filtered by a series id
+    def get_stories_by_series_id
+      # v1/public/series/{seriesId}/stories
+    end
+
+    # Stories:
+
+    # fetches lists of stories
+    def get_stories
+      # v1/public/stories
+    end
+
+    # fetches a single comic story by id
+    def get_story
+      # v1/public/stories/{storyId}
+    end
+
+     # fetches lists of characters filtered by a story id
+    def get_characters_by_story_id
+      # v1/public/stories/{storyId}/characters
+    end
+
+    # fetches lists of comics filtered by a story id
+    def get_comics_by_story_id
+      # v1/public/stories/{storyId}/comics
+    end
+
+    # fetches lists of creators filtered by a story id
+    def get_creators_by_story_id
+      # v1/public/stories/{storyId}/creators
+    end
+
+    # fetches lists of events filtered by a story id
+    def get_events_by_story_id
+      # v1/public/stories/{storyId}/events
+    end
   end
 end
