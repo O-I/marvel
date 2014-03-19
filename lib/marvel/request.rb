c3d5e728f2ca707e9adc0fb9918bb01c311ff9a4
Rahul Horé <hore.rahul@gmail.com>
2014-03-19 17:58:09 -0400

    Moves auth methods to request.rb

--- lib/marvel/request.rb
+++ lib/marvel/request.rb
@@ -6,11 +6,25 @@ module Marvel
 
     private
 
-    def request(method, path, options)
+    def request(method, path, options = {})
       response = connection.send(method) do |request|
-        request.url(path, options)
+        request.url(path, options.merge(auth))
       end
       response.body
     end
+
+    def auth
+      ts = timestamp
+      hsh = hash(ts)
+      { ts: ts, apikey: api_key, hash: hsh }
+    end
+
+    def hash(ts)
+      Digest::MD5.hexdigest(ts + private_key + api_key)
+    end
+
+    def timestamp
+      Time.now.to_s
+    end    
   end
 end
