47f9562d1c93e50c815a9ddab4a6cf1a756f7bfb
Rahul Horé <hore.rahul@gmail.com>
2014-05-21 10:42:46 -0400

    Adds Marvel#configure test

--- spec/marvel_api_spec.rb
+++ spec/marvel_api_spec.rb
@@ -6,4 +6,16 @@ describe 'Marvel' do
       expect(Marvel.new).to be_a Marvel::Client
     end
   end
+
+  describe '.configure' do
+    it 'sets the api_key and private_key' do
+      Marvel.configure do |config|
+        config.api_key = 'API_KEY'
+        config.private_key = 'PRIVATE_KEY'
+      end
+
+      expect(Marvel.api_key).to eq 'API_KEY'
+      expect(Marvel.private_key).to eq 'PRIVATE_KEY'
+    end
+  end
 end
