a4aaa225587fb1cc03d31e1e1e1aae930dac8fe4
Rahul Horé <hore.rahul@gmail.com>
2014-05-22 16:16:25 -0400

    Edits marvel_api_spec.rb

--- spec/marvel_api_spec.rb
+++ spec/marvel_api_spec.rb
@@ -1,6 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe 'Marvel' do
+describe Marvel do
   describe '.new' do
     it 'returns a Marvel::Client' do
       expect(Marvel.new).to be_a Marvel::Client
@@ -10,12 +10,12 @@ describe 'Marvel' do
   describe '.configure' do
     it 'sets the api_key and private_key' do
       Marvel.configure do |config|
-        config.api_key = 'API_KEY'
-        config.private_key = 'PRIVATE_KEY'
+        config.api_key = '1234'
+        config.private_key = 'abcd'
       end
 
-      expect(Marvel.api_key).to eq 'API_KEY'
-      expect(Marvel.private_key).to eq 'PRIVATE_KEY'
+      expect(Marvel.api_key).to eq '1234'
+      expect(Marvel.private_key).to eq 'abcd'
     end
   end
 end
