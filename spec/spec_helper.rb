45d2c44705d9f6e033f4d2ff87f43912c3c8404d
Rahul Horé <hore.rahul@gmail.com>
2014-05-22 18:23:38 -0400

    Adds mock & stub machinery for testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,16 +1,54 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
+
 require 'rspec'
+require 'webmock/rspec'
 require 'marvel_api'
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
 RSpec.configure do |config|
-  
+  config.expect_with(:rspec) { |c| c.syntax = :expect }
 end
 
-Marvel.configure do |config|
-  
+WebMock.disable_net_connect!(allow_localhost: true)
+
+def marvel_test_client
+  Marvel::Client.new.tap do |client|
+    client.configure do |config|
+      config.api_key = '1234'
+      config.private_key = 'abcd'
+    end
+  end
+end
+
+AUTH = '?ts=1&apikey=1234&hash=ffd275c5130566a2916217b101f26150'
+
+def stub_get(path, options = {})
+  file = options.fetch(:returns)
+  stub_request(:get, Marvel::Connection::BASE_API_URL + path + AUTH).
+    to_return(body: fixture(file))
+end
+
+def fixture_path
+  File.expand_path('../fixtures', __FILE__)
+end
+
+def fixture(file)
+  File.new(fixture_path + '/' + file)
+end
+
+module Marvel
+  module Request
+    private
+    def auth
+      {
+        ts: '1',
+        apikey: api_key,
+        hash: hash('1')
+      }
+    end
+  end
 end
