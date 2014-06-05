76d259590727ffb84ab7fdf3d8d5b3216c1ab0a4
Rahul Horé <hore.rahul@gmail.com>
2014-06-05 19:04:31 -0400

    Updates #stub_get and #fixture to support etag tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,11 +39,14 @@ AUTH = '?ts=1&apikey=1234&hash=ffd275c5130566a2916217b101f26150'
 def stub_get(path, options = {})
   file = options.delete(:returns)
   code = options.delete(:response_code) || 200
+  etag = options.delete(:etag)
+  headers = Marvel::Connection::HEADERS
+  headers.merge!('If-None-Match' => etag) if etag
   params = '&' + options.map { |k, v| "#{k}=#{v}" } * '&'
   endpoint = Marvel::Connection::BASE_API_URL + path + AUTH + params
-  stub_request(:get, endpoint).
-    with(headers: Marvel::Connection::HEADERS).
-    to_return(body: fixture(file), status: code)
+  stub_request(:get, endpoint)
+    .with(headers: headers)
+    .to_return(body: fixture(file), status: code)
 end
 
 def fixture_path
@@ -51,7 +54,7 @@ def fixture_path
 end
 
 def fixture(file)
-  File.new(fixture_path + '/' + file)
+  File.new(fixture_path + '/' + file) rescue nil
 end
 
 module Marvel
